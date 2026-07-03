45818d45b16a39f8b7ced814de4923a6b3ef849c
Uyi Ighodaro <141790084+UIghodaro@users.noreply.github.com>
2026-07-03 20:43:56 +0100

    Add: top-k-elements-in-list - submission-2

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-2.py
@@ -0,0 +1,17 @@
+# idea: 
+# 1. Hash map, count elements in 1 pass
+# 2. Some kind of data structures maintain what the top k are
+# 3. Return what 
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = defaultdict(int)
+        sortedKeys = []
+
+        for item in nums:
+            count[item] += 1
+
+        # Sort the keys in reverse order
+        sortedKeys = sorted(count.keys(), key = count.get, reverse = True)
+        
+        return sortedKeys[:k]
