ce3bc859581a63ea26a13e7f010dc2ded1d13a4a
Uyi Ighodaro <141790084+UIghodaro@users.noreply.github.com>
2026-07-12 00:19:12 +0100

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.py
@@ -0,0 +1,34 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        out = ""
+        for string in strs:
+            out += str(len(string)) + '#' + string
+
+        return out 
+
+    def decode(self, s: str) -> List[str]:
+        out = []
+        point = 0
+
+        while point < len(s) - 1:
+            num = ''
+
+            # When you meet a # move 1 forward and then start eating
+            while s[point] != '#':
+                num += s[point]
+                point += 1
+
+            point += 1      # Skip the #
+            string = ""     
+            num = int(num)
+
+            # Begin eating
+            for i in range(num):
+                string += s[point]
+                point += 1
+
+            out.append(string)
+
+        return out
+
